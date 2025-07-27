4b334c80a883db454fc93a69b9bf0aa2de17496f
Vladislav Kotletkin <drzhkn1@mail.ru>
2025-07-27 12:43:54 +0300

    feat: released all tests, but not released checks with feign

--- commerce/shopping-cart/src/main/java/ru/practicum/mapper/ShoppingCartMapper.java
+++ commerce/shopping-cart/src/main/java/ru/practicum/mapper/ShoppingCartMapper.java
@@ -1,6 +1,7 @@
 package ru.practicum.mapper;
 
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 import org.mapstruct.MappingConstants;
 import ru.practicum.model.ShoppingCart;
 import ru.practicum.store.ShoppingCartDto;
@@ -8,5 +9,7 @@ import ru.practicum.store.ShoppingCartDto;
 @Mapper(componentModel = MappingConstants.ComponentModel.SPRING)
 public interface ShoppingCartMapper {
 
+    @Mapping(target = "products", source = "items")
+    @Mapping(target = "shoppingCartId", source = "id")
     ShoppingCartDto toDto(ShoppingCart shoppingCart);
 }
