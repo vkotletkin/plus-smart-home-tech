5599a7daeddabb74d328469a55a49b6c620d6f3a
Vladislav Kotletkin <drzhkn1@mail.ru>
2025-08-08 11:10:00 +0300

    fix: review fixes

--- commerce/interaction-api/src/main/java/ru/practicum/feign/warehouse/WarehouseFallback.java
+++ commerce/interaction-api/src/main/java/ru/practicum/feign/warehouse/WarehouseFallback.java
@@ -12,38 +12,40 @@ import java.util.UUID;
 @Component
 public class WarehouseFallback implements WarehouseFeignClient {
 
+    private static final String WAREHOUSE_SERVICE_UNAVAILABLE = "Warehouse Service Unavailable";
+
     @Override
     public void createProductToWarehouse(NewProductInWarehouseRequest request) {
-        throw new WarehouseInternalServerException("Warehouse service unavailable");
+        throw new WarehouseInternalServerException(WAREHOUSE_SERVICE_UNAVAILABLE);
     }
 
     @Override
     public BookedProductsDto checkProductToWarehouse(ShoppingCartDto shoppingCartDto) {
-        throw new WarehouseInternalServerException("Warehouse service unavailable");
+        throw new WarehouseInternalServerException(WAREHOUSE_SERVICE_UNAVAILABLE);
     }
 
     @Override
     public void addProductToWarehouse(AddProductToWarehouseRequest request) {
-        throw new WarehouseInternalServerException("Warehouse service unavailable");
+        throw new WarehouseInternalServerException(WAREHOUSE_SERVICE_UNAVAILABLE);
     }
 
     @Override
     public AddressDto getAddress() {
-        throw new WarehouseInternalServerException("Warehouse service unavailable");
+        throw new WarehouseInternalServerException(WAREHOUSE_SERVICE_UNAVAILABLE);
     }
 
     @Override
     public void shippedToDelivery(DeliveryRequest request) {
-        throw new WarehouseInternalServerException("Warehouse service unavailable");
+        throw new WarehouseInternalServerException(WAREHOUSE_SERVICE_UNAVAILABLE);
     }
 
     @Override
     public void acceptReturn(Map<UUID, Integer> products) {
-        throw new WarehouseInternalServerException("Warehouse service unavailable");
+        throw new WarehouseInternalServerException(WAREHOUSE_SERVICE_UNAVAILABLE);
     }
 
     @Override
     public BookedProductsDto assemblyProductsForOrder(AssemblyRequest request) {
-        throw new WarehouseInternalServerException("Warehouse service unavailable");
+        throw new WarehouseInternalServerException(WAREHOUSE_SERVICE_UNAVAILABLE);
     }
 }
--- commerce/interaction-api/src/main/java/ru/practicum/order/ProductReturnRequest.java
+++ commerce/interaction-api/src/main/java/ru/practicum/order/ProductReturnRequest.java
@@ -13,7 +13,7 @@ import java.util.UUID;
 @FieldDefaults(level = AccessLevel.PRIVATE)
 public class ProductReturnRequest {
 
-    @NotEmpty
+    @NotNull
     String userName;
 
     @NotNull
